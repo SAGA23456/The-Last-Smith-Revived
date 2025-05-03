37a6632632d323500289708d34cb3b172213305c
Isaac Thompson <risinginiris2017@gmail.com>
2025-05-03 11:33:54 +0800

    20250503-1 Temporarily remove Cauldron HEI compat

--- src/main/java/cn/mmf/lastsmith/recipe/jei/cauldron/package-info.java
+++
@@ -1,8 +0,0 @@
-@ParametersAreNonnullByDefault
-@FieldsAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package cn.mmf.lastsmith.recipe.jei.cauldron;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-import mcp.MethodsReturnNonnullByDefault;
-import mezz.jei.util.FieldsAreNonnullByDefault;
